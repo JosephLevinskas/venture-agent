fa5c4698f37d7fdb3c72628ebb7fcd3efc72b9b2
Joey Levinskas <levinskasiv@gmail.com>
2026-06-10 13:12:09 -0500

    Add projects CRUD API

---
+++ backend/app/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timezone # need to store timestamps 
+
+from sqlalchemy import DateTime, Integer, String, Text # database column types 
+from sqlalchemy.orm import Mapped, mapped_column # Mapped[...] = Python-side type hint, mapped_column(...) = database-side column definition
+
+from app.database import Base 
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+    DateTime(timezone=True),
+    default=lambda: datetime.now(timezone.utc),
+    nullable=False,
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
